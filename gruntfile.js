366e6b5fce5e040a6415501af708f2ac959f63ba
Martin Surynek <surynekmartin@gmail.com>
2015-07-10 20:21:14 +0200

    simplify gruntfile
    to manage of loading task by grunt tasks plugin

--- gruntfile.js
+++ gruntfile.js
@@ -213,23 +213,7 @@ module.exports = function(grunt) {
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-processhtml');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-ng-annotate');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-htmlhint');
-	grunt.loadNpmTasks('grunt-beep');
-
-	// Load plugins
-	//require("matchdep")
-	//	.filterDev("grunt-*")
-	//	.forEach(grunt.loadNpmTasks);
+	require('load-grunt-tasks')(grunt);
 
 	grunt.registerTask('cleanBuild', ['clean:empty']);
 	grunt.registerTask('deploy', [
